efadabb2ec47d3a4dc5f387a008d400bb49f5b90
dias-danilo <StylleEz@gmail.com>
2024-07-22 17:06:01 -0300

    Iniciando JavaScript

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,9 @@ const hamburguer = document.getElementById('hamburguer')
 
 hamburguer.addEventListener('click',()=>{ 
     nav.classList.toggle('toggle');
+    if(nav.className == 'toggle') {
+        hamburguer.innerHTML = '<i class="fa fa-close"></i>';
+    } else {
+        hamburguer.innerHTML = '<i class="fa fa-bars"></i>';
+    }
 })
